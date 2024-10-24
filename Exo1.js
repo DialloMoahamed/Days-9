2008862de29c742bd51188f0f24029714cc9d93f
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-24 15:43:46 +0100

    Question 10 terminer

--- Exo1.js
+++ Exo1.js
@@ -71,3 +71,16 @@ console.log(numbersCarré);
 // reponse numero 9
 const nameToUpperCase = names.map((majuscule) => majuscule.toUpperCase())
 console.log(nameToUpperCase);
+
+
+// reponse numero 10
+const products = [
+    { product: 'banana', price: 3 },
+    { product: 'mango', price: 6 },
+    { product: 'potato', price: ' ' },
+    { product: 'avocado', price: 8 },
+    { product: 'coffee', price: 10 },
+    { product: 'tea', price: '' },
+  ]
+const correspondingPrices = products.map((cor) => cor.price)
+console.log(correspondingPrices);
